ff7f37957711c2333f2a53aeaccc914c4afad956
suxxus <suxxus@outlook.com>
2020-05-14 14:04:04 +0200

    Add Switcher to App

--- src/App.js
+++ src/App.js
@@ -7,22 +7,27 @@ import {withRouter} from "react-router-dom";
 import {NavLink} from "react-router-dom";
 
 export class App extends React.Component {
-  isDude() {
-    return this.props?.match?.params?.filter === "dude";
-  }
-
   getTitle() {
-    return this.isDude() ? "Write something Dude!" : "Hey Dude!";
+    const filter = this.props?.match?.params?.filter ?? "";
+    switch (filter) {
+      case "home":
+        return "You are in Home Dude";
+      case "dude":
+        return "Write something Dude!";
+      default:
+        return "Welcome Dude!";
+    }
   }
 
   render() {
     return (
-      <div>
+      <div style={{margin: "0 auto", width: "80%"}}>
         <h1>{this.getTitle()}</h1>
         <nav className="footer">
           <NavLink exact to="/home">
             to home
           </NavLink>
+          <span style={{margin: "5px"}}>|</span>
           <NavLink exact to="/dude">
             to dude
           </NavLink>
