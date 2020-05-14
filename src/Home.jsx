75798c212c398d191db25367e72af60552ebccd3
suxxus <suxxus@outlook.com>
2020-05-14 14:01:40 +0200

    Create Home section

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {withRouter} from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  );
+};
+
+export default withRouter(Home);
